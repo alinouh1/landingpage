8f7f58377018e65b77723bb942f5a9697f2b29b6
alinouh <86925883+alinouh@users.noreply.github.com>
2026-08-17 16:09:24 +0300

    Updates

---
+++ _next/static/chunks/3kh5ij1bw9nsj.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,11521,t=>{t.v(e=>Promise.all(["static/chunks/1kq508tkz284e.js"].map(e=>t.l(e))).then(()=>e(30733)))}]);
